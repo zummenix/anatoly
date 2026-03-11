23e5f64be27e9428f7b436fa67550b07d236b82a
Aleksey Kuznetsov <zummenix@gmail.com>
2026-03-11 08:37:55 +0500

    Add tests for safe-shell tool

--- src/tools/safe_shell.rs
+++ src/tools/safe_shell.rs
@@ -70,3 +70,106 @@ fn run_safe_shell_cmd(command: &str) -> Result<String, SafeShellToolError> {
         ))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{fs, path::PathBuf};
+
+    use super::*;
+    use insta::assert_snapshot;
+    use temp_dir::TempDir;
+
+    #[tokio::test]
+    async fn tool_definition() {
+        let def = SafeShellTool.definition(String::from("prompt")).await;
+        assert_snapshot!(serde_json::to_string_pretty(&def).unwrap(), @r#"
+        {
+          "name": "safe-shell",
+          "description": "Runs safe shell commands like cat, grep, find, etc.",
+          "parameters": {
+            "$schema": "https://json-schema.org/draft/2020-12/schema",
+            "title": "SafeShellToolArgs",
+            "type": "object",
+            "properties": {
+              "cmd": {
+                "type": "string"
+              }
+            },
+            "required": [
+              "cmd"
+            ]
+          }
+        }
+        "#);
+    }
+
+    struct FileEnv {
+        temp_dir: TempDir,
+        #[allow(dyn_drop)]
+        insta_settings_bind_drop_guard: Option<Box<dyn Drop>>,
+    }
+
+    impl FileEnv {
+        fn new() -> Self {
+            Self {
+                temp_dir: TempDir::new().expect("create TempDir"),
+                insta_settings_bind_drop_guard: None,
+            }
+        }
+
+        fn setup_insta_filter(&mut self) {
+            let mut settings = insta::Settings::clone_current();
+            settings.add_filter(&self.temp_dir.path().to_string_lossy(), "[TEMP_DIR]");
+            self.insta_settings_bind_drop_guard = Some(Box::new(settings.bind_to_scope()));
+        }
+
+        fn write_file(&self, path: &str, contents: &[u8]) -> PathBuf {
+            let full_path = self.temp_dir.child(path);
+            std::fs::write(&full_path, contents).expect("write file");
+            full_path
+        }
+    }
+
+    #[tokio::test]
+    async fn cat_command_is_safe() {
+        let file_env = FileEnv::new();
+        let path = file_env.write_file("text.txt", "hello\n  world".as_bytes());
+
+        let tool = SafeShellTool;
+        let result = tool
+            .call(SafeShellToolArgs {
+                cmd: format!("cat {}", path.to_string_lossy()),
+            })
+            .await
+            .expect("call success");
+        assert_snapshot!(result, @r"
+        hello
+          world
+        ");
+    }
+
+    #[tokio::test]
+    async fn cat_command_is_denied() {
+        let mut file_env = FileEnv::new();
+        file_env.setup_insta_filter();
+        let path = file_env.write_file("text.txt", "hello\n  world".as_bytes());
+        let output_path = file_env.write_file("output.txt", "something".as_bytes());
+
+        let tool = SafeShellTool;
+        let result = tool
+            .call(SafeShellToolArgs {
+                cmd: format!(
+                    "cat {} >> {}",
+                    path.to_string_lossy(),
+                    output_path.to_string_lossy()
+                ),
+            })
+            .await
+            .expect_err("call failure");
+        assert_snapshot!(result.to_string(), @r"Command 'cat [TEMP_DIR]/text.txt >> [TEMP_DIR]/output.txt' is not allowed or is restricted");
+        assert_eq!(
+            fs::read_to_string(output_path).expect("read output file"),
+            "something"
+        );
+    }
+}
